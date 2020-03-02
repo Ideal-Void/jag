e32ec1e0134af4a9fee2f14bf9f26f7746ae1e30
Carlos Donizete Froes <coringao@riseup.net>
2020-03-02 11:28:30 -0300

    Updated the revision of the source file for the current year

--- src/editor/levelpack.cpp
+++ src/editor/levelpack.cpp
@@ -6,12 +6,12 @@
  * Many thanks to XlabSoft & Ind. Infosystems, the originals authors of JAG.
  *
  * Copyright holder 2009-2012 Developed by XlabSoft & Industrial Infosystems
- * Work continued by 2017-2018 Carlos Donizete Froes [a.k.a coringao]
+ * Work continued by 2017-2020 Carlos Donizete Froes [a.k.a coringao]
  *
  * JAG is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * (at your option) any later version.
  *
  * JAG is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
